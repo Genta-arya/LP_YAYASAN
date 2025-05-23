25028148272d5d7f1184694e2d45b27a3f14b010
Genta-arya <mgentaarya@gmail.com>
2025-05-23 10:18:56 +0700

    add robot 2

--- src/app/(Landing-Page)/(Beranda)/Maps.jsx
+++ src/app/(Landing-Page)/(Beranda)/Maps.jsx
@@ -15,14 +15,14 @@ const LokasiMap = () => {
         </p>
         <div className="w-full h-[450px]">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.6935569092625!2d109.98706487396109!3d-1.8699742981129548!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e0517bb491e0f8f%3A0x7ebeddc66452417e!2sYayasan%20Islamiyah%20Al-Jihad%20Ketapang!5e0!3m2!1sen!2sid!4v1742572946148!5m2!1sen!2sid"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.6935569092625!2d109.98706487396109!3d-1.8699742981129548!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e0517bb491e0f8f%3A0x7ebeddc66452417e!2sYayasan%20Islamiyah%20Al-Jihad%20Ketapang!5e0!3m2!1sen!2sid!4v1742572946148!5m2!1sen!2sid&maptype=satellite"
             width="100%"
             height="100%"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          />
         </div>
       </div>
     </section>
