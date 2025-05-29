83b664f76549f65c4c4555416f72889f820483e4
Genta-arya <mgentaarya@gmail.com>
2025-05-30 00:14:47 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,48 +7,6 @@ const Footer = () => {
     <footer className="bg-green-800 text-white py-8 border-t-8 border-yellow-400  ">
       <div className="px-3 xl:px-8  ">
        
-        <div className=" mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
-          {/* Logo or Title */}
-          <Image
-          src="/Logoweb.png"
-          alt="Logo"
-          width={400}
-          height={300}
-          className="mb-2 bg-white"
-        />
-          {/* Navigation Links */}
-          <div className="flex justify-center space-x-6">
-           
-          </div>
-
-          {/* Social Media Icons */}
-          <div className="flex justify-center md:justify-end space-x-4">
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <FiFacebook size={20} />
-            </a>
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <FiInstagram size={20} />
-            </a>
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-gray-300"
-            >
-              <FiYoutube size={20} />
-            </a>
-          </div>
-        </div>
       </div>
 
       {/* Copyright */}
